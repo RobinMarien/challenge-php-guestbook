b2ba1618c99e91388aa97948f86cb8d79eaeb336
RobinMarien <dev.robinmarien@gmail.com>
2019-10-22 17:04:19 +0200

    added amount selector

--- view/home.php
+++ view/home.php
@@ -28,6 +28,13 @@
                     <textarea id="content" name="content" placeholder="Your message here!"></textarea>
                     <label for="author">Your Name:</label>
                     <input type="text" id="author" name="author" placeholder="Your name here!">
+                    <label for="display">How many posts to display:</label>
+                    <select name = "display">
+                        <option>5</option>
+                        <option>10</option>
+                        <option>15</option>
+                        <option>20</option>
+                    </select>
                     <button type="submit">Post</button>
                 </form>
             </div>
@@ -35,18 +42,33 @@
         <div class="row">
             <div class="col-md-12">
                 <?php
-                for ($x = 0; $x <= 20; $x++) {
-                    echo "<div><h4>"
-                        . $allPosts[$x]->getTitle()
-                        . "</h4><p>"
-                        . $allPosts[$x]->getDate()
-                        . "</p><p>"
-                        . $allPosts[$x]->getContent()
-                        . "</p><p>"
-                        . $allPosts[$x]->getAuthor()
-                        . "</p></div>";
-
+                if (isset($_POST['display'])){
+                    for ($x = 0; $x <= $_POST['display']; $x++) {
+                        echo "<div><h4>"
+                            . $allPosts[$x]->getTitle()
+                            . "</h4><p>"
+                            . $allPosts[$x]->getDate()
+                            . "</p><p>"
+                            . $allPosts[$x]->getContent()
+                            . "</p><p>"
+                            . $allPosts[$x]->getAuthor()
+                            . "</p></div>";
+                    }
                 }
+                else {
+                    for ($x = 0; $x <= 20; $x++) {
+                        echo "<div><h4>"
+                            . $allPosts[$x]->getTitle()
+                            . "</h4><p>"
+                            . $allPosts[$x]->getDate()
+                            . "</p><p>"
+                            . $allPosts[$x]->getContent()
+                            . "</p><p>"
+                            . $allPosts[$x]->getAuthor()
+                            . "</p></div>";
+                    }
+                }
+
                 ?>
             </div>
         </div>
